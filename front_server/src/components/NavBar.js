cdf5165b29555bf2eba8f68cbb5c7b41255c3a92
Gustavo Lisboa Molina <g.lisboa@edu.pucrs.br>
2024-10-24 03:34:48 -0300

    integrated v0.0.1

---
+++ front_server/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './views/NavBar.css'; // Use this CSS for styling
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/home2">HomePage2</Link>
+                <Link to="/home3">HomePage3</Link>
+                <Link to="/home4">HomePage4</Link>
+                <Link to="/home5">HomePage5</Link>
+                <Link to="/home9">HomePage9</Link>
+                <Link to="/pesqcd">PesqCD</Link>
+                <Link to="/cadastracd">CadastraCD</Link>
+                <Link to="/pesquisaAbrigo">Pesquisa Abrigo</Link>
+                <Link to="/cadastraAbrigo">Cadastra Abrigo</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
